79f636721375904603f128883ca585c0b11bea68
qier222 <qier222@outlook.com>
2021-07-10 15:50:49 +0800

    Replace google font with self-host font

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,17 @@ import { createGlobalStyle, ThemeProvider } from "styled-components"
 import splitbee from "@splitbee/web"
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400 500 600 700;
+    font-display: optional;
+    src: url("/fonts/inter-var-latin.woff2") format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+      U+FEFF, U+FFFD;
+  }
+
   body {
     margin: 0;
     padding: 0;
--- pages/_document.js
+++ pages/_document.js
@@ -36,15 +36,12 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            href="/fonts/inter-var-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
